20c7166e21a6c1f4572ac1990f74813fe3646f06
Neofitka <neofitka-el@yandex.ru>
2023-03-29 21:21:39 +0300

    Ver2

--- 15/15.cpp
+++ 15/15.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <conio.h> 
 #include <cstdio> 
+#include <ctime>
+using namespace std;
 
 int Field[4][4];
 int EmptyX, EmptyY;
@@ -11,36 +13,102 @@ int EmptyX, EmptyY;
 enum Direction { LEFT, UP, RIGHT, DOWN };
 
 void CreateField();
+void CreateField_(int N);
 void DrawField();
-void Move(Direction dir);
+void DrawField_(int N);
+void Move(Direction dir, int N);
 bool FieldIsCorrect();
+bool FieldIsCorrect_(int N);
+void showMenu();
 
 int main()
 {
-    CreateField();
-    DrawField(); 
-    char c; 
-    int a;
-    while (!FieldIsCorrect())
+    setlocale(LC_ALL, "russian");
+ 
+    
+    int choice = 0;
+    unsigned int start_time, end_time, search_time;
+
+    while (choice != 3)
     {
-        // c = getchar();
-        std::cin >> a;
-        switch (a)
+        unsigned int start_time, end_time, search_time;
+        system("cls");
+        showMenu();
+        cin >> choice;
+        switch (choice)
         {
-        case 4 /*75*/: Move(LEFT); break; 
-        case 8/*72*/: Move(UP); break;
-        case 6/*77*/: Move(RIGHT); break;
-        case 2/*80*/: Move(DOWN); break;
-        case 0/*27*/: return 0;
+        case 1:
+            unsigned int start_time = clock();
+            CreateField_(3);
+            DrawField_(3);
+            char c;
+            while (!FieldIsCorrect_(3))
+            {
+                c = _getch();
+                switch (c)
+                {
+                case 75: Move(LEFT, 3); break;
+                case 72: Move(UP, 3); break;
+                case 77: Move(RIGHT, 3); break;
+                case 80: Move(DOWN, 3); break;
+                case 27: return 0;
+                }
+                DrawField_(3);
+            }
+            end_time = clock();
+            search_time = end_time - start_time; 
+
+            std::cout << "\n\nВаше время:"<< search_time;
+            std::cout << "\n\nПобеда! Нажмите Enter для выхода!";
+            cout << endl;
+            system("pause");
+            std::cin.get();
+            break;
+
+        case 2:
+            unsigned int start_time_4 = clock();
+            CreateField_(4);
+            DrawField_(4);
+            char cc;
+            while (!FieldIsCorrect_(4))
+            {
+                cc = _getch();
+                switch (cc)
+                {
+                case  75: Move(LEFT, 4); break;
+                case 72: Move(UP, 4); break;
+                case 77: Move(RIGHT, 4); break;
+                case 80: Move(DOWN, 4); break;
+                case 27: return 0;
+                }
+                DrawField_(4);
+            }
+            unsigned int end_time_4 = clock();
+            unsigned int search_time_4 = end_time_4 - start_time_4;
+
+            std::cout << "\n\nВаше время:" << search_time_4;
+            std::cout << "\n\nПобеда! Нажмите Enter для выхода!";
+            cout << endl;
+            system("pause");
+            std::cin.get();
+            break;
+        case 27: return 0;
+
         }
-        DrawField(); 
     }
-    std::cout << "\n\nCongratulations! Press Enter to exit!";
-    std::cin.get();
 }
 
+void showMenu() {
 
+    std::cout << "Игра Пятнашки" << std::endl;
+    std::cout << std:: endl;
+    std::cout << "[1]  8 (3х3)" << std::endl;
+    std::cout << "[2]  15 (4х4)" << std::endl;
+    std::cout << "[3]  Выход." << std::endl;
+    std::cout << std::endl;
+    std::cout << "Выберите режим игры:" << std::endl;
 
+}
 
 void CreateField()
 {
@@ -83,11 +151,61 @@ void CreateField()
     }
 
     for (int i = 0; i < 15; i++)
-        Field[i % 4][i / 4] = Nums[i];
+        Field[i % 4/2][i / 4] = Nums[i];
     Field[3][3] = 0;
     EmptyX = 3; EmptyY = 3;
 
 }
+
+void CreateField_(int N) // N= 3 или 4
+{
+    int size = N * N - 1;
+    bool* NumIsFree = new bool[size];
+    int* Nums = new int[size];
+    for (int i = 0; i < size; i++)
+        NumIsFree[i] = true;
+
+    srand(time(NULL));
+
+    bool Ok;
+    int RandNum;
+    for (int i = 0; i < size; i++)
+    {
+        Ok = false;
+        while (!Ok)
+        {
+            RandNum = rand() % size + 1;
+            if (NumIsFree[RandNum - 1])
+                Ok = true;
+        }
+        Nums[i] = RandNum;
+        NumIsFree[RandNum - 1] = false;
+    }
+
+    int Chaos = 0;
+    int CurrNum;
+    for (int i = 0; i < size-1; i++)
+    {
+        CurrNum = Nums[i];
+        for (int j = i + 1; j < size; j++)
+            if (CurrNum > Nums[j])
+                Chaos++;
+    }
+    if (Chaos % 2 == 1)
+    {
+        int temp = Nums[size-2];
+        Nums[size-2] = Nums[size-1];
+        Nums[size-1] = temp;
+    }
+
+    for (int i = 0; i < size; i++) {
+    Field[i % N][i / N] = Nums[i];
+    }
+    Field[N-1][N-1] = 0;
+    EmptyX = N-1; EmptyY = N-1;
+
+}
+
 
 void DrawField()
 {
@@ -126,13 +244,50 @@ void DrawField()
     }
 }
 
-void Move(Direction dir)
+void DrawField_(int N)
+{
+    system("cls");
+
+    for (int j = 0; j < N; j++)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            if (Field[i][j])
+                std::cout << "----";
+            else
+                std::cout << "    ";
+        }
+        std::cout << '\n';
+        for (int i = 0; i < N; i++)
+        {
+            if (Field[i][j])
+            {
+                std::cout << "|";
+                std::cout.width(2);
+                std::cout << Field[i][j] << "|";
+            }
+            else
+                std::cout << "    ";
+        }
+        std::cout << '\n';
+        for (int i = 0; i < N; i++)
+        {
+            if (Field[i][j])
+                std::cout << "----";
+            else
+                std::cout << "    ";
+        }
+        std::cout << '\n';
+    }
+}
+
+void Move(Direction dir, int N)
 {
     switch (dir)
     {
     case LEFT:
     {
-        if (EmptyX < 3)
+        if (EmptyX < N-1)
         {
             Field[EmptyX][EmptyY] = Field[EmptyX + 1][EmptyY];
             Field[EmptyX + 1][EmptyY] = 0;
@@ -141,7 +296,7 @@ void Move(Direction dir)
     } break;
     case UP:
     {
-        if (EmptyY < 3)
+        if (EmptyY < N-1)
         {
             Field[EmptyX][EmptyY] = Field[EmptyX][EmptyY + 1];
             Field[EmptyX][EmptyY + 1] = 0;
@@ -177,7 +332,14 @@ bool FieldIsCorrect()
     return true;
 }
 
-
+bool FieldIsCorrect_(int N)
+{
+    int size = N * N - 1;
+    for (int i = 0; i < size; i++)
+        if (Field[i % N][i / N] != i + 1)
+            return false;
+    return true;
+}
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
