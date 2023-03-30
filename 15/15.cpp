39b7b1c0dc55698974864ed1d90028d099d9735b
ZaharovArt <zaharov-art@mail.ru>
2023-03-30 22:57:25 +0300

    End Version

--- 15/15.cpp
+++ 15/15.cpp
@@ -12,23 +12,18 @@ int EmptyX, EmptyY;
 
 enum Direction { LEFT, UP, RIGHT, DOWN };
 
-void CreateField();
 void CreateField_(int N);
-void DrawField();
 void DrawField_(int N);
 void Move(Direction dir, int N);
 bool FieldIsCorrect();
 bool FieldIsCorrect_(int N);
 void showMenu();
+void OutTime(float sec_time);
 
 int main()
 {
     setlocale(LC_ALL, "russian");
- 
-    
     int choice = 0;
-    unsigned int start_time, end_time, search_time;
-
     while (choice != 3)
     {
         unsigned int start_time, end_time, search_time;
@@ -37,8 +32,8 @@ int main()
         cin >> choice;
         switch (choice)
         {
-        case 1:
-            unsigned int start_time = clock();
+        case 1: {
+            int start_time = clock();
             CreateField_(3);
             DrawField_(3);
             char c;
@@ -55,18 +50,21 @@ int main()
                 }
                 DrawField_(3);
             }
-            end_time = clock();
-            search_time = end_time - start_time; 
+            int end_time = clock();
+            int search_time = end_time - start_time;
+            float sec_time = ((float)search_time) / CLOCKS_PER_SEC;
+            int min_time;
+            
+            OutTime(sec_time);
 
-            std::cout << "\n\nВаше время:"<< search_time;
-            std::cout << "\n\nПобеда! Нажмите Enter для выхода!";
+            cout << "\n\nПобеда! Нажмите Enter для выхода!";
             cout << endl;
             system("pause");
-            std::cin.get();
+            cin.get(); }
             break;
 
-        case 2:
-            unsigned int start_time_4 = clock();
+        case 2: {
+            int start_time = clock();
             CreateField_(4);
             DrawField_(4);
             char cc;
@@ -83,14 +81,18 @@ int main()
                 }
                 DrawField_(4);
             }
-            unsigned int end_time_4 = clock();
-            unsigned int search_time_4 = end_time_4 - start_time_4;
+            int end_time = clock();
+            int search_time = end_time - start_time;
+            float sec_time = ((float)search_time) / CLOCKS_PER_SEC;
+            int min_time;
 
-            std::cout << "\n\nВаше время:" << search_time_4;
-            std::cout << "\n\nПобеда! Нажмите Enter для выхода!";
+            OutTime(sec_time);
+
+            cout << "\n\nПобеда! Нажмите Enter для выхода!";
             cout << endl;
             system("pause");
-            std::cin.get();
+            cin.get();
+        }
             break;
         case 27: return 0;
 
@@ -106,58 +108,11 @@ void showMenu() {
     std::cout << "[2]  15 (4х4)" << std::endl;
     std::cout << "[3]  Выход." << std::endl;
     std::cout << std::endl;
-    std::cout << "Выберите режим игры:" << std::endl;
-
+    std::cout << "Выберите режим игры: ";
 }
 
-void CreateField()
-{
-    bool NumIsFree[15]; 
-    int Nums[15]; 
-    for (int i = 0; i < 15; i++) 
-        NumIsFree[i] = true;
 
-    srand(time(NULL));
-
-    bool Ok;
-    int RandNum;
-    for (int i = 0; i < 15; i++)
-    {
-        Ok = false;
-        while (!Ok)
-        {
-            RandNum = rand()%15 + 1; 
-            if (NumIsFree[RandNum - 1])
-                Ok = true;
-        }
-        Nums[i] = RandNum; 
-        NumIsFree[RandNum - 1] = false;
-    }
-
-    int Chaos = 0; 
-    int CurrNum; 
-    for (int i = 0; i < 14; i++) 
-    {
-        CurrNum = Nums[i];
-        for (int j = i + 1; j < 15; j++)
-            if (CurrNum > Nums[j])
-                Chaos++;
-    }
-    if (Chaos % 2 == 1) 
-    { 
-        int temp = Nums[13];
-        Nums[13] = Nums[14];
-        Nums[14] = temp;
-    }
-
-    for (int i = 0; i < 15; i++)
-        Field[i % 4/2][i / 4] = Nums[i];
-    Field[3][3] = 0;
-    EmptyX = 3; EmptyY = 3;
-
-}
-
-void CreateField_(int N) // N= 3 или 4
+void CreateField_(int N) // N = 3 или 4
 {
     int size = N * N - 1;
     bool* NumIsFree = new bool[size];
@@ -182,7 +137,7 @@ void CreateField_(int N) // N= 3 или 4
         NumIsFree[RandNum - 1] = false;
     }
 
-    int Chaos = 0;
+    int Chaos = 0; // Число беспорядков
     int CurrNum;
     for (int i = 0; i < size-1; i++)
     {
@@ -191,7 +146,7 @@ void CreateField_(int N) // N= 3 или 4
             if (CurrNum > Nums[j])
                 Chaos++;
     }
-    if (Chaos % 2 == 1)
+    if (Chaos % 2 == 1) 
     {
         int temp = Nums[size-2];
         Nums[size-2] = Nums[size-1];
@@ -207,42 +162,6 @@ void CreateField_(int N) // N= 3 или 4
 }
 
 
-void DrawField()
-{
-    system("cls");
-
-    for (int j = 0; j < 4; j++)
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            if (Field[i][j])
-                std::cout << "----";
-            else
-                std::cout << "    ";
-        }
-        std::cout << '\n';
-        for (int i = 0; i < 4; i++)
-        {
-            if (Field[i][j])
-            {
-                std::cout << "|";
-                std::cout.width(2);
-                std::cout << Field[i][j] << "|";
-            }
-            else
-                std::cout << "    ";
-        }
-        std::cout << '\n';
-        for (int i = 0; i < 4; i++)
-        {
-            if (Field[i][j])
-                std::cout << "----";
-            else
-                std::cout << "    ";
-        }
-        std::cout << '\n';
-    }
-}
 
 void DrawField_(int N)
 {
@@ -340,13 +259,12 @@ bool FieldIsCorrect_(int N)
             return false;
     return true;
 }
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+void OutTime(float sec_time) {
+    int min_time;
+    if (sec_time < 60) cout << "\n\nВаше время:" << (int)sec_time << " сек.";
+    if (sec_time > 60 && sec_time < 3600) {
+        min_time = sec_time / 60;
+        cout << "\n\nВаше время: " << min_time << " мин. " << (int)sec_time - min_time * 60 << " сек.";
+    }
+};
